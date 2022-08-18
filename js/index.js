7e490dda8dd19163db87f27afa8628742bf28597
sk8higher <sk8higher@iballwasrawt.ru>
2022-08-18 18:21:59 +0300

    Adding classes for Application and player state

--- js/index.js
+++ js/index.js
@@ -1,7 +1,11 @@
 import { Gameboard } from './classes/gameboard.js';
+import { Player } from './classes/player.js';
+import { App } from './classes/app.js';
 
 const cells = document.querySelectorAll('.canvas-part');
 const cellContainer = document.querySelector('.canvas');
+
+const currentPlayer = document.querySelector('.header-player-turn');
 
 const gameboard = new Gameboard(cells);
 
