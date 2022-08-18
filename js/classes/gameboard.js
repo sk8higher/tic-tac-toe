c859e2f03e6816f9a6d5a81964837e44cc2b83eb
sk8higher <sk8higher@iballwasrawt.ru>
2022-08-19 00:12:41 +0300

    Add check canvas methods, refactoring

--- js/classes/gameboard.js
+++ js/classes/gameboard.js
@@ -14,6 +14,61 @@ class Gameboard {
       el.textContent = this._board[i];
     });
   }
+
+  checkEveryElement(arr) {
+    return (
+      arr.every((cell) => cell === 'X') || arr.every((cell) => cell === 'O')
+    );
+  }
+
+  /**
+   * Checks if there is a won line
+   * @param cellMarkers
+   * @returns {boolean}
+   */
+  checkForLines(cellMarkers) {
+    const firstLine = [cellMarkers[0], cellMarkers[1], cellMarkers[2]];
+    const secondLine = [cellMarkers[3], cellMarkers[4], cellMarkers[5]];
+    const thirdLine = [cellMarkers[6], cellMarkers[7], cellMarkers[8]];
+
+    return (
+      this.checkEveryElement(firstLine) ||
+      this.checkEveryElement(secondLine) ||
+      this.checkEveryElement(thirdLine)
+    );
+  }
+
+  /**
+   * Checks if there is a won row
+   * @param cellMarkers
+   * @returns {boolean}
+   */
+  checkForRows(cellMarkers) {
+    const firstRow = [cellMarkers[0], cellMarkers[3], cellMarkers[6]];
+    const secondRow = [cellMarkers[1], cellMarkers[4], cellMarkers[7]];
+    const thirdRow = [cellMarkers[2], cellMarkers[5], cellMarkers[8]];
+
+    return (
+      this.checkEveryElement(firstRow) ||
+      this.checkEveryElement(secondRow) ||
+      this.checkEveryElement(thirdRow)
+    );
+  }
+
+  /**
+   * Checks if there a won diagonal
+   * @param cellMarkers
+   * @returns {boolean}
+   */
+  checkForDiagonals(cellMarkers) {
+    const firstDiagonal = [cellMarkers[0], cellMarkers[4], cellMarkers[8]];
+    const secondDiagonal = [cellMarkers[2], cellMarkers[4], cellMarkers[6]];
+
+    return (
+      this.checkEveryElement(firstDiagonal) ||
+      this.checkEveryElement(secondDiagonal)
+    );
+  }
 }
 
 export { Gameboard };
--- js/index.js
+++ js/index.js
@@ -16,11 +16,10 @@ const playerX = new Player('X', 0);
 const playerO = new Player('O', 1);
 
 /*
- * gameState = 0, if game hasn't ended;
- * gameState = 1, if X wins
- * gameState = -1, if O wins
+ * isWon = false, if game is not completed
+ * isWon = true, if game is completed
  */
-let _gameState = 0;
+let _isWon = false;
 let _currentPlayer = playerX;
 
 // function _init(board) {
@@ -33,12 +32,15 @@ let _currentPlayer = playerX;
 function checkForWin() {
   const cellMarkers = Array.from(cells).map((el) => el.textContent);
 
-  console.log(cellMarkers);
+  return (
+    gameboard.checkForLines(cellMarkers) ||
+    gameboard.checkForRows(cellMarkers) ||
+    gameboard.checkForDiagonals(cellMarkers)
+  );
 }
 
 function operateCells(e) {
   // Adding current player marker on canvas
-
   // Guard clause to add markers only on empty cells
   if (cells.item(+e.target.dataset.cellNumber).textContent) return;
 
@@ -46,7 +48,12 @@ function operateCells(e) {
   gameboard.changeCellMarker(cells);
   e.target.removeEventListener('click', this);
 
-  checkForWin();
+  _isWon = checkForWin();
+  console.log(_isWon);
+
+  if (_isWon) {
+    currentPlayerText.innerHTML = `${_currentPlayer.mark} won!`;
+  }
 
   // Changing current player
   _currentPlayer = _currentPlayer._playerNumber === 0 ? playerO : playerX;
